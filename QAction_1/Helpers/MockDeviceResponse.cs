a2fa133bbbc60829008a320a119ded44d057f346
Arman Hadžigrahić <arman.hadzigrahic@skyline.be>
2026-06-15 16:10:06 +0200

    fixed more comments

--- QAction_1/Helpers/MockDeviceResponse.cs
+++ QAction_1/Helpers/MockDeviceResponse.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace QAction_1.Helpers
+﻿namespace QAction_1.Helpers
 {
 
 
-     public class MockDeviceResponse
+    public class MockDeviceResponse
     {
-    
+
         public readonly string json = @"
 {
     ""transport_streams"": [{
